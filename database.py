3a21e8e81eb4aa5cc65330ac88bebac475b2fe1f
João Vitor <docputs@gmail.com>
2020-05-29 02:15:30 -0300

    added database handler

---
+++ database.py
@@ -0,0 +1,28 @@
+from csv import DictReader, DictWriter
+
+
+def check_user_in_database(userid):
+    with open('users.csv', 'r') as arquivo:
+        leitor = DictReader(f=arquivo)
+        for linha in leitor:
+            if linha['userid'] == str(userid):
+                return True
+        return False
+
+
+def register(chatid, userid):
+    try:
+        if check_user_in_database(userid) is False:
+            with open('users.csv', 'a') as arquivo:
+                escritor = DictWriter(f=arquivo, fieldnames=['chatid', 'userid'])
+                escritor.writerow({'chatid': chatid, 'userid': userid})
+        else:
+            return False
+    except Exception as error:
+        return error
+    else:
+        return True
+
+
+if __name__ == '__main__':
+    print(register(12345, 45643))
