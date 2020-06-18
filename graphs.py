1bc0d0fd0fa6b351664c3c6fc196ba11cc7fc93d
João Vitor <docputs@gmail.com>
2020-06-18 04:19:00 -0300

    included headless chrome driver

--- graphs.py
+++ graphs.py
@@ -1,6 +1,7 @@
 import base64
-from selenium import webdriver
 from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def execute_script(driver, image):
@@ -8,10 +9,19 @@ def execute_script(driver, image):
 
 
 def download_graphs():
-    driver = webdriver.Chrome('/home/jovi/Downloads/chromedriver')
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+
+    print('Abrindo o navegador...')
+    driver = webdriver.Chrome(chrome_options=chrome_options,
+                              executable_path='/home/jovi/Downloads/chromedriver')
+    print('Acessando o site...')
     driver.get('https://covid.saude.gov.br')
     sleep(5)
 
+    print('Iniciando coleta de elementos...')
     image = driver.find_element_by_xpath(
         '/html/body/app-root/ion-app/ion-router-outlet/app-home/ion-content/painel-geral-component/div/div[3]/div[1]/div[1]/chart-bars-component/div/div/canvas'
     )
@@ -24,27 +34,32 @@ def download_graphs():
     image4 = driver.find_element_by_xpath(
         '/html/body/app-root/ion-app/ion-router-outlet/app-home/ion-content/painel-geral-component/div/div[10]/div[1]/div[1]/chart-line-component/div/div/canvas'
     )
+    print('Elementos coletados com sucesso!')
 
-    # get the image as a PNG base64 string
+    print('Iniciando conversão da imagem...')
     image_base64 = execute_script(driver, image)
     image2_base64 = execute_script(driver, image2)
     image3_base64 = execute_script(driver, image3)
     image4_base64 = execute_script(driver, image4)
+    print('Imagens convertidas com sucesso!')
 
     driver.close()
+    print('Navegador fechado')
 
-    # decode
+    print('Decodificando bytes...')
     graph1 = base64.b64decode(image_base64)
     graph2 = base64.b64decode(image2_base64)
     graph3 = base64.b64decode(image3_base64)
     graph4 = base64.b64decode(image4_base64)
     graphs = {'graph1': graph1, 'graph2': graph2,
               'graph3': graph3, 'graph4': graph4}
+    print('Bytes decodificados com sucesso!')
 
-    # save to a file
+    print('Salvando em arquivo...')
     for name, img in graphs.items():
         with open(f'images/{name}.jpg', 'wb') as f:
             f.write(img)
+    print('Arquivos salvos com sucesso!')
 
 
 if __name__ == '__main__':
