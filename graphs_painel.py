8f054209560655dceb2c11961f06fc9c59297887
João Vitor <docputs@gmail.com>
2020-06-20 13:03:41 -0300

    improved abstraction

--- graphs_painel.py
+++ graphs_painel.py
@@ -5,75 +5,63 @@ from matplotlib.ticker import MaxNLocator
 
 
 class Grafico:
-    def __init__(self, show_graph=False):
+    def __init__(self):
         self.__df = self.__read_data()
         self.__filename = 'default'
-        self.__show_graph = show_graph
-        self.__title = 'Título'
-        self.__color = 'b'
+        self.__fig = plt.figure(figsize=(14, 8))
+        self.__ax = plt.subplot()
 
     def casos_acumulados(self, semilog=False):
         df = self.__df['casosAcumulado']
-        self.__title = 'Casos acumulados de Covid-19 por data de notificação'
+        title = 'Casos acumulados de Covid-19 por data de notificação'
         self.__filename = 'casos-acumulados'
-        self.__color = 'g'
+        color = 'g'
         if semilog:
-            self.__semilog_plot(df)
+            self.__semilog_plot(df, title, color)
         else:
-            self.__line_plot(df)
+            self.__line_plot(df, title, color)
 
     def casos_novos(self):
         df = self.__df['casosNovos']
-        self.__title = 'Casos novos de Covid-19 por data de notificação'
+        title = 'Casos novos de Covid-19 por data de notificação'
         self.__filename = 'casos-novos'
-        self.__color = 'g'
-        self.__bar_plot(df)
+        color = 'g'
+        self.__bar_plot(df, title, color)
 
     def obitos_acumulados(self, semilog=False):
         df = self.__df['obitosAcumulado']
-        self.__title = 'Óbitos acumulados de Covid-19 por data de notificação'
+        title = 'Óbitos acumulados de Covid-19 por data de notificação'
         self.__filename = 'obitos-acumulados'
-        self.__color = 'm'
+        color = 'm'
         if semilog:
-            self.__semilog_plot(df)
+            self.__semilog_plot(df, color)
         else:
-            self.__line_plot(df)
+            self.__line_plot(df, color)
 
     def obitos_novos(self):
         df = self.__df['obitosNovos']
-        self.__title = 'Óbitos novos de Covid-19 por data de notificação'
+        title = 'Óbitos novos de Covid-19 por data de notificação'
         self.__filename = 'obitos-novos'
-        self.__color = 'm'
-        self.__bar_plot(df)
+        color = 'm'
+        self.__bar_plot(df, title, color)
 
-    def __semilog_plot(self, df):
+    def __semilog_plot(self, df, title, color='b'):
         self.__filename += '-log'
-        plt.semilogy(df)
-        plt.title(self.__title + ' (escala logarítmica)')
-        plt.gcf().set_size_inches(12, 7)
-        self.save_as_png()
-        if self.__show_graph:
-            plt.show()
+        self.__ax.semilogy(df, color=color)
+        self.__ax.set_title(title + ' (escala logarítmica)')
+        self.__ax.xaxis.set_major_formatter(DateFormatter('%d/%m'))
 
-    def __line_plot(self, df):
-        plt.plot(df, linewidth=4.0, color=self.__color)
-        plt.title(self.__title)
-        plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
-        plt.gca().xaxis.set_major_formatter(DateFormatter('%d/%m'))
-        plt.gcf().set_size_inches(12, 7)
-        self.save_as_png()
-        if self.__show_graph:
-            plt.show()
+    def __line_plot(self, df, title, color):
+        self.__ax.plot(df, linewidth=4.0, color=color)
+        self.__ax.set_title(title)
+        self.__ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+        self.__ax.xaxis.set_major_formatter(DateFormatter('%d/%m'))
 
-    def __bar_plot(self, df):
-        plt.bar(df.index, df, color=self.__color)
-        plt.title(self.__title)
-        plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
-        plt.gca().xaxis.set_major_formatter(DateFormatter('%d/%m'))
-        plt.gcf().set_size_inches(12, 7)
-        self.save_as_png()
-        if self.__show_graph:
-            plt.show()
+    def __bar_plot(self, df, title, color):
+        self.__ax.bar(df.index, df, color=color)
+        self.__ax.set_title(title)
+        self.__ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+        self.__ax.xaxis.set_major_formatter(DateFormatter('%d/%m'))
 
     def __read_data(self):
         df = pd.read_excel('~/Desktop/painel-covid.xlsx')
@@ -90,36 +78,43 @@ class Grafico:
         self.obitos_novos()
 
     def casos_full(self):
-        df1 = self.__df['casosAcumulado']
-        plt.subplot('211')
-        plt.plot(df1, color='g')
-        plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
-        plt.gca().xaxis.set_major_formatter(DateFormatter('%d/%m'))
-        plt.title(self.__title)
-        df2 = self.__df['casosNovos']
-        plt.subplot('212')
-        plt.bar(df2.index, df2, color='g')
-        plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
-        plt.gca().xaxis.set_major_formatter(DateFormatter('%d/%m'))
-        plt.title(self.__title)
-        plt.gcf().set_size_inches(12, 7)
-        self.__filename = 'casos-full'
-        self.save_as_png()
-        if self.__show_graph:
-            plt.show()
+        self.__fig.set_size_inches(14, 14)
 
-    def obitos(self):
-        pass
+        self.__ax = plt.subplot('211')
+        self.casos_acumulados()
+
+        self.__ax = plt.subplot('212')
+        self.casos_novos()
+
+        self.__filename = 'casos-full'
+
+    def obitos_full(self):
+        self.__fig.set_size_inches(14, 14)
+
+        self.__ax = plt.subplot('211')
+        df1 = self.__df['obitosAcumulado']
+        title = 'Óbitos acumulados de Covid-19 por data de notificação'
+        color = 'm'
+        self.__line_plot(df1, title, color)
+
+        self.__ax = plt.subplot('212')
+        df2 = self.__df['obitosNovos']
+        title = 'Óbitos novos de Covid-19 por data de notificação'
+        color = 'm'
+        self.__bar_plot(df2, title, color)
+
+        self.__filename = 'obitos-full'
 
     def save_as_png(self):
         plt.savefig(f'images/{self.__filename}.png')
-        print(f'Arquivo <{self.__filename}> salvo com sucesso!')
+        print(f'Arquivo <{self.__filename}> salvo.')
         self.__filename = 'default'
 
 
 if __name__ == '__main__':
-    graficos = Grafico(show_graph=True)
+    graficos = Grafico()
     graficos.casos_full()
+    graficos.save_as_png()
 
     # x = range(10, 100, 10)
     # y = list(map(lambda x: x**2, x))
