9cbd989d235f45a97e18f9e6de0e9d444fdf9328
João Vitor <docputs@gmail.com>
2020-06-20 03:02:01 -0300

    added option to create graphs from official xlsx data

---
+++ graphs_painel.py
@@ -0,0 +1,108 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+from matplotlib.dates import DateFormatter
+from matplotlib.ticker import MaxNLocator
+
+
+class Grafico:
+    def __init__(self, show_graph=False):
+        self.__df = self.__read_data()
+        self.__filename = 'default'
+        self.__show_graph = show_graph
+        self.__title = 'Título'
+        self.__color = 'b'
+
+    def casos_acumulados(self, semilog=False):
+        df = self.__df['casosAcumulado']
+        self.__title = 'Casos acumulados de Covid-19 por data de notificação'
+        self.__filename = 'casos-acumulados'
+        self.__color = 'g'
+        if semilog:
+            self.__semilog_plot(df)
+        else:
+            self.__line_plot(df)
+
+    def casos_novos(self):
+        df = self.__df['casosNovos']
+        self.__title = 'Casos novos de Covid-19 por data de notificação'
+        self.__filename = 'casos-novos'
+        self.__color = 'g'
+        self.__bar_plot(df)
+
+    def obitos_acumulados(self, semilog=False):
+        df = self.__df['obitosAcumulado']
+        title = 'Óbitos acumulados de Covid-19 por data de notificação'
+        self.__filename = 'obitos-acumulados'
+        self.__color = 'm'
+        if semilog:
+            self.__semilog_plot(df)
+        else:
+            self.__line_plot(df)
+
+    def obitos_novos(self):
+        df = self.__df['obitosNovos']
+        title = 'Óbitos novos de Covid-19 por data de notificação'
+        self.__filename = 'obitos-novos'
+        self.__color = 'm'
+        self.__bar_plot(df)
+
+    def __semilog_plot(self, df):
+        self.__filename += '-log'
+        plt.semilogy(df)
+        plt.title(self.__title)
+        self.save_as_png()
+        if self.__show_graph:
+            plt.show()
+
+    def __line_plot(self, df):
+        plt.plot(df, linewidth=4.0, color=self.__color)
+        plt.title(self.__title)
+        plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
+        plt.gca().xaxis.set_major_formatter(DateFormatter('%d/%m'))
+        self.save_as_png()
+        if self.__show_graph:
+            plt.show()
+
+    def __bar_plot(self, df):
+        plt.bar(df.index, df, color=self.__color)
+        plt.title(self.__title)
+        plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
+        plt.gca().xaxis.set_major_formatter(DateFormatter('%d/%m'))
+        self.save_as_png()
+        if self.__show_graph:
+            plt.show()
+
+    def __read_data(self):
+        df = pd.read_excel('~/Desktop/painel-covid.xlsx')
+        df = df[['data', 'regiao', 'casosAcumulado',
+                 'casosNovos', 'obitosAcumulado', 'obitosNovos']]
+        brasil_df = df[df['regiao'] == 'Brasil'].set_index('data')
+        print('Dados lidos com sucesso!')
+        return brasil_df
+
+    def all_graphs(self):
+        self.casos_acumulados()
+        self.casos_novos()
+        self.obitos_acumulados()
+        self.obitos_novos()
+
+    def save_as_png(self):
+        plt.savefig(f'images/{self.__filename}.png')
+        print(f'Arquivo <{self.__filename}> salvo com sucesso!')
+        self.__filename = 'default'
+
+
+if __name__ == '__main__':
+    graficos = Grafico(show_graph=True)
+    graficos.all_graphs()
+    # x = range(10, 100, 10)
+    # y = list(map(lambda x: x**2, x))
+
+    # plt.figure(1)
+    # plt.subplot('211')
+    # plt.plot(x, y)
+    # plt.xlabel('eixo x')
+
+    # plt.subplot('212')
+    # plt.plot(y, x)
+    # plt.show()
