62e685e9bbc9f2a1ced2b20d67901773c5f11e9e
João Vitor <docputs@gmail.com>
2020-07-01 20:59:31 -0300

    fixed translation bugs

--- country_ranking.py
+++ country_ranking.py
@@ -7,27 +7,34 @@ translation = {
     'Brazil': 'Brasil',
     'India': 'India',
     'Russian Federation': 'Rússia',
-    'United Kingdom': 'Reino Unido'
+    'United Kingdom': 'Reino Unido',
+    'Peru': 'Peru',
+    'Chile': 'Chile',
+    'Spain': 'Espanha',
+    'Italy': 'Itália',
+    'Iran, Islamic Republic of': 'Irã'
 }
+
 
 def get_ranking_from_json():
     with open('country_ranking.json') as f:
         dados = json.load(f)
-    print(dados)
-    texto = f"""{'País':<30}{'Casos confirmados'}
-{dados['Country'][str(0)]:<30}{dados['TotalConfirmed'][str(0)]}
-{dados['Country'][str(1)]:<30}{dados['TotalConfirmed'][str(1)]}
-    """
-    print(texto)
+    texto = f'{"País":<20}Casos confirmados\n'
+    for j in range(len(dados)):
+        texto += f'{dados["Country"][str(j)]:<20}{dados["TotalConfirmed"][str(j)]}\n'
+    return texto
+
 
 # monta um ranking com base nos dados atuais e salva em arquivo JSON
 if __name__ == '__main__':
     r = requests.get('https://api.covid19api.com/summary')
     dados = r.json()['Countries']
     df_full = pd.DataFrame.from_dict(dados).sort_values(
-        by='TotalConfirmed', ascending=False).head(5).reset_index()
+        by='TotalConfirmed', ascending=False).head(10).reset_index()
     df = df_full[['Country', 'CountryCode', 'TotalConfirmed', 'NewConfirmed',
                   'TotalDeaths', 'NewDeaths', 'TotalRecovered', 'NewRecovered', 'Date']]
+    print(df)
+    df.loc[:, 'Country'] = df['Country'].apply(lambda x: translation[x])
     df.to_json('country_ranking.json')
 
     print(get_ranking_from_json())
